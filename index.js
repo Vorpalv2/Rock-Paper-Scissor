b1c137f0b0cdea5975a34f01a75e4e61c92f2766
Aakash <aakashanand1995@gmail.com>
2023-04-18 22:42:50 +0530

    more edits

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //button click --> DoM update --> check if else condition
+var choice = ["Rock","Paper","Scissor"];
+var playerChoice = prompt("Enter your choice");
 
 document.querySelector("#rockButton").addEventListener("click",function(){
     //alert("check");
@@ -6,13 +8,43 @@ document.querySelector("#rockButton").addEventListener("click",function(){
 })
 
 
-
-var choice = ["Rock","Paper","Scissor"];
-
-
 function computerChoice(){
     var RandomNum = Math.floor(Math.random()*3);
     var compChose = choice[RandomNum];
-    console.log(RandomNum);
+    // console.log(RandomNum);
+    // console.log(compChose);
     return compChose;
+
 }
+
+function game (intake){
+   // console.log(intake);
+    if (intake==="Rock"){
+        return "computer chose Rock"
+    }
+    else if (intake ==="Paper"){
+        return "computer chose paper"
+    }
+    else{
+        return "computer chose scissor"
+    }
+}
+
+function playerChose(intakePlayer){
+    var intaketoupper = intakePlayer.toUpperCase();
+    console.log(intaketoupper);
+
+    if(intaketoupper === "ROCK")
+        console.log("Player chose Rock");
+
+    else if (intaketoupper == "PAPER"){
+        console.log("Player chose Paper");
+    }
+    else{
+        console.log("Player chose Scissor");
+    }
+}
+
+playerChose(playerChoice);
+
+//game(computerChoice(choice));
